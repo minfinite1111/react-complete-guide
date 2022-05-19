ddb65aebd5d970497716bb1df7cf00004b9fb51a
reptar lakins <minfinite1111@gmail.com>
2022-05-18 22:06:17 -0700

    64. created stateful filter component w/ two way binding

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,20 +1,33 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
-import Card from './Card'
+import Card from "./Card";
+import ExpensesFilter from "./NewExpense/ExpensesFilter";
+import React, { useState } from "react";
 
 function Expenses(props) {
+  const [enteredYear, setEnteredYear] = useState("2020");
+
+  const filterChangeHandler = (enteredFilterValue) => {
+    console.log("change");
+    console.log(enteredFilterValue);
+    setEnteredYear(enteredFilterValue);
+  };
+
   return (
-    <Card className="expenses">
-      {props.items.map((items) => {
-        return (
-          <ExpenseItem
-            title={items.title}
-            amount={items.amount}
-            date={items.date}
-          ></ExpenseItem>
-        );
-      })}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={enteredYear} onSaveFilterChange={filterChangeHandler} />
+        {props.items.map((items) => {
+          return (
+            <ExpenseItem
+              title={items.title}
+              amount={items.amount}
+              date={items.date}
+            ></ExpenseItem>
+          );
+        })}
+      </Card>
+    </div>
   );
 }
 
